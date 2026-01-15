6e6993e97ae83ac5fc0d7ef191406f26a6e6734c
AYUSH ADARSH <ayush01adarsh@gmail.com>
2026-01-15 16:11:41 +0530

    Update assignment_2b.py

--- assignment_2b.py
+++ assignment_2b.py
@@ -1,8 +1,6 @@
-from turtledemo.penrose import start
-from turtledemo.round_dance import stop
-
 total_sum =0
 for num in range(1,51):
     total_sum = total_sum + num
 print(f"The sum of numbers from 1 to 50 is: {total_sum}")
+
 
